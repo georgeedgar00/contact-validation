62425b2f5e30a80017a2fdac5c75ac199007f45d
George Edgar <georgeedgar00@gmail.com>
2022-11-26 21:28:33 -0600

    js update

--- assets/script/index.js
+++ assets/script/index.js
@@ -7,77 +7,45 @@ const cityRegex = /^(?:[A-Za-z]{2,}(?:(\.\s|'s\s|\s?-\s?|\s)?(?=[A-Za-z]+))){1,2
 
 const add = select('.create-btn');
 const parent = select('.contact-page');
-const contactInfo = select(".contact-input");
+const contactInput = select(".contact-input");
 const contactCounter = select('.num-contact p')
-const errorOutput = select('.message')
+const errorMessage = select('.message')
 
-/*****************************************
-        Creating new Contact
-*****************************************/
+
 function newContact(name, city, email) {
     const createContact = new Contact(name, city, email)
     return createContact
 };
 
 
-/*****************************************
-        Building Contact
-*****************************************/
 
-// Empty array
 const arr = []
 
-function createContact(obj){
+function makeContact(obj){
    
-    const contactInput = contactInfo.value.split(', ');
+    const contactInput = contactInput.value.split(', ');
 
-    if(contactInfo.value == '') {
-        errorOutput.innerText = 'Enter the fields above!'
+    if(contactInput.value == '') {
+        errorMessage.innerText = 'Enter the fields above!';
     } else {
-        let contactDiv = create('div');
+        let contactDiv = createElement('div');
         contactDiv.className = 'contact';
         parent.prepend(contactDiv);
         arr.push(obj)
         contactCounter.innerText = `Contacts: ${arr.length}`
-        errorOutput.innerText = 'Contact Created!';
+        errorMessage.innerText = 'Contact Created!';
 
-        // Delete contact
         contactDiv.onclick = function() {
             contactDiv.remove()
             contactCounter.innerText = `Contacts: ${arr.length -= 1}`
         }
-        contactDiv.innerText = `Name: ${contactInput[0]}\nCity: ${contactInput[1]}\n Email: ${contactInput[2]}`
+        contactDiv.innerText = `Name: ${contactInput[0]}, City: ${contactInput[1]}, Email: ${contactInput[2]}`
     }
 }
 
 
-/*****************************************
-        Assigning new Contact
-*****************************************/
 
-function assignContact(){
-        if(contactInfo.value !== ''){
-            const contactInput = contactInfo.value.split(', ');
-
-            // Email Validation
-            if(!emailRegex.test(contactInput[2])){
-                errorOutput.innerText = 'Email is not valid. Delete and try again.'
-            }else if (!cityRegex.test(contactInput[1])) {
-                errorOutput.innerText = 'City is not valid. Delete and try again.'
-            } else if(!nameRegex.test(contactInput[0])){
-                errorOutput.innerText = 'Name is not valid. Delete and try again.'
-                
-            }
-        } else {
-            errorOutput.innerText = 'Full Name, City and Email are required.';
-        }
-
-}
-
-/*****************************************
-        onEvent add
-*****************************************/
 
 onEvent('click', add, function(){
-    event.preventDefault(assignContact(createContact(newContact(contactInfo.value))))
+    event.preventDefault(makeContact(newContact(contactInput.value)))
 })
